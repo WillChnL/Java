77cd87c725230f0ca3bedd0dc9a7f9be8bd5fb03
William Chan <34170384+WillChnL@users.noreply.github.com>
2018-06-18 21:48:37 -0700

    Added java file

---
+++ Helloworld.java
@@ -0,0 +1,48 @@
+//Exercises P1.5
+
+
+public class ProblemSet1
+{
+   public static void main(String[] args)
+     
+   {
+      System.out.println("___________");
+      System.out.println("| Richard |");
+      System.out.println("+---------+\n\n");
+   
+  //Exercises P1.6
+
+     
+      System.out.println("########  ####  ######  ##     ##    ###    ########  ######## ");
+      System.out.println("##     ##  ##  ##    ## ##     ##   ## ##   ##     ## ##     ##");
+      System.out.println("##     ##  ##  ##       ##     ##  ##   ##  ##     ## ##     ##");
+      System.out.println("########   ##  ##       ######### ##     ## ########  ##     ##");
+      System.out.println("##   ##    ##  ##       ##     ## ######### ##   ##   ##     ##");
+      System.out.println("##    ##   ##  ##    ## ##     ## ##     ## ##    ##  ##     ##");
+      System.out.println("##     ## ####  ######  ##     ## ##     ## ##     ## ########\n\n");
+     
+  //Exercises P1.7
+     
+     
+      System.out.println(" //////\\\\");
+      System.out.println("//       \\");
+      System.out.println("|   _   _ |");
+      System.out.println("|-(.)-(.)-|");
+      System.out.println("|    J    |");
+      System.out.println("\\   ---  //");
+      System.out.println("\\       //");
+      System.out.println("  ######\n\n");
+     
+  //Exercises P1.10
+     
+     
+      System.out.println(" /\\___/\\");
+      System.out.println("(  o o  )   ---------");
+      System.out.println("/   *   \\  /  Hello  \\");
+      System.out.println("\\__\\_/__/ < Professor|");
+      System.out.println("  /   \\    \\  Louie  /");
+      System.out.println(" / ___ \\    ---------");
+      System.out.println(" \\/___\\/\n\n");
+   }
+  
+}
