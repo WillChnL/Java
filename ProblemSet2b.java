48997487fb92144258825e5ed0e34636efeacdbc
William Chan <34170384+WillChnL@users.noreply.github.com>
2018-07-10 17:09:39 -0700

    simple calculator for a cashier

--- ProblemSet2b.java
+++ ProblemSet2b.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Scanner;
 public class ProblemSet2b
 {
@@ -36,43 +35,4 @@ public class ProblemSet2b
  
          
  }
-=======
-import java.util.Scanner;
-public class ProblemSet2b
-{
-  public static void main(String[] args)
- {
-    
-    Scanner KB = new Scanner(System.in);
-        
-    System.out.println("Amount due in $?");
-    int amountd = KB.nextInt();
- 
-    System.out.println("Amount received in $?");
-    int amountr = KB.nextInt();
- 
-    int difference = (amountr - amountd);
-     
-    int balance = difference;
-         
-    int dll = balance / 100;
-    balance = (balance - (100 * dll));
- 
-    int quarter = balance / 25;
-    balance = (balance - (25 * quarter));
- 
-    int dime = balance / 10;
-    balance = (balance - (10 * dime));
- 
-    int nickel = balance / 5;
-    balance = (balance - (5 * nickel));
-     
-    int pennie = balance / 1;
-    balance = balance - (1 * pennie);
- 
-    System.out.println("You need to give back " + dll + " dollar " + quarter + " quarter " + dime + " dime " + nickel + " nickel " + pennie + " pennie");
- 
-         
- }
->>>>>>> ffc120a946fe01823a2b1acf88f2a77f3784e520
 }
